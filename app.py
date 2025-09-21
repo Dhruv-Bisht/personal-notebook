2682deb2f0d2fbc432ee5e2b80aeae21d91bad56
Dhruv-Bisht <103594018+Dhruv-Bisht@users.noreply.github.com>
2025-09-21 13:36:32 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # app.py
 from flask import Flask, render_template, request, redirect, session
 import sqlite3
+import os
+
+
+
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"
@@ -59,4 +63,6 @@ def delete_note(note_id):
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # <-- Use Render's port
+    app.run(host="0.0.0.0", port=port)
+
